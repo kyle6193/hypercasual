338c4e9c5d23240954a983dc4ede5dc0bdbf4c2e
kyle6193 <kyle.transue@gmail.com>
2025-11-04 15:41:06 -0500

    add player main.tscn mouse movement

---
+++ player.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var min_x : int = 40
+var max_x : int = 500
+
+#update ship's position to mouse click
+func _input(event: InputEvent) -> void:
+	if Input.is_action_pressed("click"):
+		position.x = event.position.x
+		position.x = clamp(position.x, min_x, max_x)
